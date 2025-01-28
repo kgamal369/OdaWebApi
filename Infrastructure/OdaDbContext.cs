08f2658782d699f10c82cbad7d50f0b5617fc621
Karim Gamal Elsayed <karim.elsayed@sms-digital.com>
2025-01-28 15:27:20 +0100

    elka01:try to fix

--- Infrastructure/OdaDbContext.cs
+++ Infrastructure/OdaDbContext.cs
@@ -55,9 +55,7 @@ public partial class OdaDbContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=OdaDB;Username=postgres;Password=THEspider369;Port=5432");
-
+        => optionsBuilder.UseNpgsql("Host=dpg-cuc1s39opnds738s419g-a.oregon-postgres.render.com;Database=odadb;Username=odadb_user;Password=iwiEqjZ2mwcqFuREbb8U1GNTyfxKbgGw;Port=5432;SslMode=Require;TrustServerCertificate=True");
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Addon>(entity =>
