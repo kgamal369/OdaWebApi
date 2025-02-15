8f79818885875c6e38ae71611c720f9da474878a
Karim Elsayed <eng.karim.gamal369@gmail.com>
2025-02-15 23:25:17 +0100

    elka01:fix Data in

---
+++ Domain/DTOs/ApartmentDTO.cs
@@ -0,0 +1,10 @@
+namespace OdaWepApi.Domain.DTOs
+{
+    public class ApartmentDTO
+    {
+        public int? ApartmentId { get; set; }
+        public int? ApartmentType { get; set; }
+        public decimal? ApartmentSpace { get; set; }
+        public string? ApartmentAddress { get; set; }
+    }
+}
--- Domain/DTOs/BookingDataIn.cs
+++ Domain/DTOs/BookingDataIn.cs
@@ -1,19 +1,18 @@
-﻿using OdaWepApi.Domain.Models;
+﻿using  OdaWepApi.Domain.Models;
 
 namespace OdaWepApi.Domain.DTOs
 {
     public class BookingDataIn
     {
-        public int DeveloperID { get; set; }
-        public int ProjectID { get; set; }
-        public int? ApartmentId { get; set; }
-        public int? ApartmentType { get; set; }
-        public decimal? ApartmentSpace { get; set; }
+        public int? DeveloperID { get; set; }
+        public int? ProjectID { get; set; }
+        public ApartmentDTO apartmentDTO { get; set; }
         public int PlanID { get; set; }
         public List<AddonSelection> Addons { get; set; }
         public List<int> AddonPerRequestIDs { get; set; }
         public int? AutomationID { get; set; }
         public Customer CustomerInfo { get; set; }
         public int PaymentPlanID { get; set; }
+        public List<Questions> Questions { get; set; }
     }
 }
